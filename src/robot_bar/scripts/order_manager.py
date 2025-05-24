6652eeebdb563aa360a29250788d5a5e6122538f
NapMarta <marta.napolillo@outlook.it>
2025-05-24 23:17:31 +0200

    Rallentamento invio ordini

--- src/robot_bar/scripts/order_manager.py
+++ src/robot_bar/scripts/order_manager.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-    Nodo che legge l'ordine e lo invia al delivery_manager per gestirlo
+    Nodo che legge l'ordine e lo invia al delivery_manager
+    con un ordine ogni 10 secondi.
 """
 
 import rospy
 import rosnode
-from robot_bar.msg import Order  # Importa il tuo messaggio custom
+from robot_bar.msg import Order
 from std_msgs.msg import String
 import random
 
@@ -14,31 +15,31 @@ def callback_consegna(msg):
 
 def order_manager():
     rospy.init_node('order_manager', anonymous=True)
+    # Attendi che Gazebo sia pronto
     while not rospy.is_shutdown():
         try:
-            nodes = rosnode.get_node_names()
-            if '/gazebo' in nodes:
+            if '/gazebo' in rosnode.get_node_names():
                 break
         except:
             pass
         rospy.sleep(1)
 
-    rate = rospy.Rate(2)
+    # 0.5 Hz → 1 ordine ogni 50 secondi
+    rate = rospy.Rate(0.5)
     pub = rospy.Publisher("/ordine", Order, queue_size=10)
     rospy.Subscriber("/consegna_completata", String, callback_consegna)
 
     i = 1
     while not rospy.is_shutdown():
-    # for i in range(5):
         ordine = Order()
         ordine.id_ordine = f"ORD00{i}"
         ordine.prodotto = "Espresso"
-        ordine.tavolo = random.choice([1, 2, 3])
+        ordine.tavolo = random.choice([1, 2, 3, 4])
 
-        rospy.loginfo(f"Inviando ordine: \n {ordine}")
+        rospy.loginfo(f"Inviando ordine: \n{ordine}")
         pub.publish(ordine)
         i += 1
-        # rospy.sleep(10)  # Mantiene il nodo in esecuzione
+
         rate.sleep()
 
 if __name__ == '__main__':
